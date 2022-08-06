40efcae8941279bb33dbf2c41a95a5921bfcc29d
Sithum-Dilshan <sithumdilshan985@gmail.com>
2022-08-06 19:08:22 +0530

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,9 @@
+import {Typography} from "@mui/material";
+
+function App() {
+  return (
+    <Typography>Hello</Typography>
+  );
+}
+
+export default App;
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import {Stack} from "@mui/material";
+
+export default function Login(props) {
+    return(
+        <Stack bgcolor={'#d9d9d9'} width={'100vw'} height={'100vh'} maxWidth={'100%'}>
+            <Stack height={'70%'} width={'60%'} bgcolor={'#c7b4b4'}>
+
+            </Stack>
+        </Stack>
+    );
+}
